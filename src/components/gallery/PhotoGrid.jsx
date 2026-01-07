cd7e004408fee61168d92e598fe2c6fc85128cf9
TrelawneyJourney <the-trelawney-journey@outlook.com>
2026-01-07 09:29:31 -0300

    cambios en grilla mobile

--- src/components/gallery/PhotoGrid.jsx
+++ src/components/gallery/PhotoGrid.jsx
@@ -1,20 +1,40 @@
 export default function PhotoGrid({ images, onItemClick }) {
+  const rows = [];
+
+  for (let i = 0; i < images.length; i += 2) {
+    rows.push(images.slice(i, i + 2));
+  }
+
   return (
-    <div className="grid grid-cols-[1fr_0.7fr] md:grid-cols-[1fr_0.7fr_1fr] gap-3">
-      {images.map((item) => (
-        <button
-          key={item.id}
-          onClick={() => onItemClick?.(item)}
-          className="group overflow-hidden "
-        >
-          {/* auto-rows-min aspect-[3/4] */}
-          <img
-            src={item.src}
-            alt={item.alt}
-            className="w-full h-full transition-transform duration-500 group-hover:scale-105 object-cover "
-          />
-        </button>
-      ))}
+    <div className="grid grid-cols-1 md:grid-cols-[1fr_0.7fr_1fr] gap-3">
+      {rows.map((row, rowIndex) => {
+        const isReversed = rowIndex % 2 !== 0;
+
+        return (
+          <div
+            key={rowIndex}
+            className={`grid gap-3 ${
+              isReversed ? "grid-cols-[0.7fr_1fr]" : "grid-cols-[1fr_0.7fr]"
+            } md:contents`}
+          >
+            {row.map((item) => (
+              <button
+                key={item.id}
+                onClick={() => onItemClick?.(item)}
+                className="group overflow-hidden "
+              >
+                {/* auto-rows-min aspect-[3/4] */}
+                <img
+                  src={item.src}
+                  alt={item.alt}
+                  className="w-full h-full transition-transform duration-500 group-hover:scale-105 object-cover "
+                />
+              </button>
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
+// "grid grid-cols-[1fr_0.7fr] md:grid-cols-[1fr_0.7fr_1fr] gap-3
