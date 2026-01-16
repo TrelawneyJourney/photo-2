fe094b21e37cf24a97916e6446837e3b93284230
TrelawneyJourney <the-trelawney-journey@outlook.com>
2026-01-16 10:47:21 -0300

    add slide animation

--- src/components/gallery/CarouselImg.jsx
+++ src/components/gallery/CarouselImg.jsx
@@ -6,13 +6,43 @@ import {
   Mousewheel,
   Zoom,
 } from "swiper/modules";
+import { motion, AnimatePresence, scale } from "framer-motion";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/zoom";
+import { useState } from "react";
+
+const variants = {
+  enter: (direction) => ({
+    x: direction > 0 ? 120 : -120,
+    opacity: 0,
+    scale: 0.95,
+  }),
+  center: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+  },
+  exit: (direction) => ({
+    x: direction > 0 ? -120 : 120,
+    opacity: 0,
+    scale: 0.95,
+  }),
+};
 
 export default function CarouselImg({ photos, initialIndex, onSlideChange }) {
+  const [activeIndex, setActiveIndex] = useState(initialIndex);
+  const [direction, setDirection] = useState(0);
+
+  const handleOnSlideChange = (swiper) => {
+    const newIndex = swiper.activeIndex;
+    setDirection(newIndex > activeIndex ? 1 : -1);
+    setActiveIndex(newIndex);
+    onSlideChange(swiper);
+  };
+
   return (
     <Swiper
       modules={[Navigation, Keyboard, Pagination, Mousewheel, Zoom]}
@@ -26,20 +56,31 @@ export default function CarouselImg({ photos, initialIndex, onSlideChange }) {
         toggle: true,
       }}
       initialSlide={initialIndex}
-      onSlideChange={onSlideChange}
+      onSlideChange={handleOnSlideChange}
       spaceBetween={30}
       className="h-screen w-full [--swiper-navigation-color:#9ca3af]
     [--swiper-pagination-color:white]"
     >
-      {photos.map((photo) => (
-        <SwiperSlide key={photo.id}>
+      {photos.map((photo, index) => (
+        <SwiperSlide key={photo.slug}>
           <div className="h-full w-full flex items-center justify-center swiper-zoom-container">
-            <img
-              src={photo.src}
-              alt={photo.alt}
-              className="max-w-full max-h-full object-contain select-none"
-              draggable={false}
-            />
+            <AnimatePresence mode="wait" custom={direction}>
+              {index === activeIndex && (
+                <motion.img
+                  key={photo.slug}
+                  src={photo.src}
+                  alt={photo.alt}
+                  className="max-w-full max-h-full object-contain select-none"
+                  draggable={false}
+                  custom={direction}
+                  variants={variants}
+                  initial="enter"
+                  animate="center"
+                  exit="exit"
+                  transition={{ duration: 0.85, ease: "easeOut" }}
+                />
+              )}
+            </AnimatePresence>
           </div>
         </SwiperSlide>
       ))}
