bef8e99f5cef169d1d44b141053c527c73ecc0af
TrelawneyJourney <the-trelawney-journey@outlook.com>
2026-01-17 21:19:40 -0300

    add style form

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,78 +1,76 @@
 export default function ContactForm() {
   return (
-    <form
-      action=""
-      method="post"
-      className="flex flex-col justify-center gap-2 md:gap-4 mt-8 md:mt-0"
-    >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <label for="name" className="font-light tracking-widest hidden">
-          Nombre
-        </label>
-        <input
-          type="name"
-          name="name"
-          id="name"
-          placeholder="Nombre"
-          className="w-full py-3 px-2 bg-transparent border border-neutral-300 text-neutral-800 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
-        />
-      </div>
+    <form action="" method="post" className="max-w-[900px] ">
+      <div className="grid grid-cols-2">
+        <div className="">
+          <label for="name" className="font-light tracking-widest sr-only">
+            Nombre
+          </label>
+          <input
+            type="name"
+            name="name"
+            id="name"
+            placeholder="Nombre"
+            className="w-full py-3 px-2 bg-transparent border border-neutral-300 border-r-0 text-neutral-800 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
+          />
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <label for="email" className="font-light tracking-widest hidden">
-          Email
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-          className="w-full py-3 px-2 bg-transparent border border-neutral-300 text-neutral-800 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
-        />
-      </div>
+        <div className="">
+          <label for="email" className="font-light tracking-widest sr-only">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            className="w-full py-3 px-2 bg-transparent border border-neutral-300 text-neutral-800 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
+          />
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <label for="telefono" className="font-light tracking-widest hidden">
-          Telefóno
-        </label>
-        <input
-          type="tel"
-          name="telefono"
-          id="telefono"
-          placeholder="Telefóno"
-          className="w-full py-3 px-2 bg-transparent border border-neutral-300 text-neutral-800 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
-        />
-      </div>
+        <div className="">
+          <label for="telefono" className="font-light tracking-widest sr-only">
+            Telefóno
+          </label>
+          <input
+            type="tel"
+            name="telefono"
+            id="telefono"
+            placeholder="Telefóno"
+            className="w-full py-3 px-2 bg-transparent border border-y-0 border-neutral-300 text-neutral-800 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
+          />
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <label for="telefono" className="font-light tracking-widest hidden">
-          Asunto
-        </label>
-        <input
-          type="text"
-          name="asunto"
-          id="asunto"
-          placeholder="Asunto"
-          className="w-full py-3 px-2 bg-transparent border border-neutral-300 text-neutral-800 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
-        />
-      </div>
+        <div className="">
+          <label for="asunto" className="font-light tracking-widest sr-only">
+            Asunto
+          </label>
+          <input
+            type="text"
+            name="asunto"
+            id="asunto"
+            placeholder="Asunto"
+            className="w-full py-3 px-2 bg-transparent text-neutral-800 border-r border-neutral-300 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
+          />
+        </div>
 
-      <div className="flex flex-col">
-        <label for="message" class="font-light tracking-widest hidden">
-          Mensaje
-        </label>
-        <textarea
-          name="message"
-          id="message"
-          placeholder="Contame un poco sobre el proyecto o la idea que tenés en mente."
-          rows={6}
-          className="w-full py-3 px-2 bg-transparent border border-neutral-300 text-neutral-800 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
-        ></textarea>
+        <div className="col-span-2">
+          <label for="message" class="font-light tracking-widest sr-only">
+            Mensaje
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Contame un poco sobre el proyecto o la idea que tenés en mente."
+            rows={6}
+            className="w-full py-3 px-2 bg-transparent border border-neutral-300 text-neutral-800 font-light tracking-wide focus:outline-none focus:border-neutral-900 transition"
+          ></textarea>
+        </div>
       </div>
 
       <button
         type="submit"
-        className="self-start md:w-32 bg-neutral-900 text-white py-3 px-6 rounded-lg mt-3 hover:bg-neutral-500 transition ease-in-out duration-300"
+        className="md:w-32 bg-neutral-900 text-white text-sm tracking-widest uppercase py-3 px-8 mt-3 hover:bg-neutral-500 transition ease-in-out duration-300"
       >
         Enviar
       </button>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,14 +3,13 @@ import ContactForm from "../components/contact/ContactForm";
 export default function Contact() {
   return (
     <main>
-      <div>
-        <h1 className="text-4xl md:text-5xl">Contactame</h1>
-        <p className="mt-4 max-w-xl text-neutral-600 leading-relaxed">
-          Si te interesa trabajar juntes o tenés un proyecto en mente, podés
-          escribirme a través del formulario. Estoy disponible para nuevos
-          proyectos y colaboraciones.
-        </p>
-      </div>
+      <h1 className="text-4xl md:text-5xl mb-4">Contactame</h1>
+      <p className="mb-6 max-w-xl text-neutral-500 leading-relaxed">
+        Si te interesa trabajar juntes o tenés un proyecto en mente, podés
+        escribirme a través del formulario. Estoy disponible para nuevos
+        proyectos y colaboraciones.
+      </p>
+
       <ContactForm />
     </main>
   );
