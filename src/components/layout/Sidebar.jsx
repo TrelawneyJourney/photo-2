24e8dbb5491d67c0e55f14031e5576c1a4e4dd4e
TrelawneyJourney <the-trelawney-journey@outlook.com>
2026-01-20 17:53:33 -0300

    add overflow hidden

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,17 +1,35 @@
 import Navbar from "../navbar/Navbar";
 import FooterNavbar from "../navbar/FooterNavbar";
 import Logo from "../navbar/Logo";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { icons } from "../../assets/icons";
 
 export default function Sidebar() {
   const [mobileOpen, setMobileOpen] = useState(false);
   const MenuIcon = icons.Menu;
   const CrossIcon = icons.Cross;
+  const menuRef = useRef();
 
   const toggleMenu = () => {
     setMobileOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    const handleClickOut = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setMobileOpen(false);
+      }
+    };
+    if (mobileOpen) {
+      document.addEventListener("mousedown", handleClickOut);
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOut);
+    };
+  }, [mobileOpen]);
 
   return (
     <>
@@ -19,8 +37,8 @@ export default function Sidebar() {
         <div className="px-4">
           <Logo />
         </div>
-        <button onClick={toggleMenu} className=" p-4">
-          <MenuIcon />
+        <button onClick={toggleMenu} className="z-50 p-4">
+          {mobileOpen ? <CrossIcon /> : <MenuIcon />}
         </button>
       </div>
 
@@ -28,14 +46,11 @@ export default function Sidebar() {
         <div
           className="fixed inset-0 bg-black/30 z-30 md:hidden flex justify-end items-start p-4"
           onClick={toggleMenu}
-        >
-          <button onClick={toggleMenu}>
-            <CrossIcon />
-          </button>
-        </div>
+        ></div>
       )}
 
       <aside
+        ref={menuRef}
         className={`fixed inset-y-0 left-0 z-40 w-64 p-6 space-y-5 bg-white duration-300 ease-out md:static md:translate-x-0 ${
           mobileOpen ? "translate-x-0" : "-translate-x-full"
         }`}
--- src/components/navbar/FooterNavbar.jsx
+++ src/components/navbar/FooterNavbar.jsx
@@ -2,10 +2,10 @@ import { icons } from "../../assets/icons";
 
 export default function FooterNavbar() {
   const InstaIcon = icons.Instagram;
-  const LinkedinIcon = icons.Linkedin;
   const WhatsappIcon = icons.Whatsapp;
+
   return (
-    <div className="flex items-center gap-2 mt-7">
+    <div className="flex items-center gap-3 mt-7">
       <a
         href="https://www.instagram.com/galacaradepala_fotografia/"
         target="_blank"
@@ -13,6 +13,7 @@ export default function FooterNavbar() {
       >
         <InstaIcon />
       </a>
+
       <a href="#" target="_blank" rel="noopener noreferrer">
         <WhatsappIcon />
       </a>
